942023a1129f88f54e5210006b5738672c8353d5
fernibain <fernibain5@gmail.com>
2023-01-18 17:43:25 -0700

    First commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Montana</h1>
+        <br/>
+        <div className="table">
+          
+        </div>
+        <br/>
+        <div className="deck">
+        
+        </div>
+        <br/>
+
+        <div className="table">
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
